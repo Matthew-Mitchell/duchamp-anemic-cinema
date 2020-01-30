e4306026733b98ca58b305f2854075937d7d1bd4
Matthew-Mitchell <mmitchell17@protonmail.com>
2020-01-30 18:38:38 -0500

    Updated centers rotating speeds. Uses 2 different speeds, A and B and applies A to even circles and B to odd circles; these produce a spiral like effect. 5/7 is an interesting ratio.

--- tribute-to-duchamp-anemic-cinema.js
+++ tribute-to-duchamp-anemic-cinema.js
@@ -1,6 +1,6 @@
 new p5();
 
-let min=100;
+let min=60;
 let max=600;
 let step=20;
 var centers=[];
@@ -14,23 +14,23 @@ for ( let r=max; r>=min; r+= -step) {
 function setup() {
   createCanvas(1000,1000);
   smooth();
-  strokeWeight(1); // Default
-  strokeWeight(4); // Thicker
+  strokeWeight(2); // Default
   }
 var angle = 0;
-var speed = 0.0025;
+var speed = 0.005;
 function draw(){
   let count = 0;
+  // angle += speed;
   for ( let r=max; r>=min; r+= -step) {
-    angle += speed;
+    angle += (speed )
     let sinval = sin(angle);
     let cosval = cos(angle);
     if ((r%(2*step))==0){
-        fill(255);
+        fill(0);
         var x = centers[count][0] + sinval*4;
         var y = centers[count][1] + cosval*4;
       }
-        else {fill(0);
+        else {fill(255);
              var x = centers[count][0] + sinval*3;
             var y = centers[count][1] + cosval*3;}   
     ellipse(x,y,r,r);
