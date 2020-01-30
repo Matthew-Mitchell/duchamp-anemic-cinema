06a5796ce79bb21646fa69accbe3109344e4fe0a
Matthew-Mitchell <mmitchell17@protonmail.com>
2020-01-30 18:11:11 -0500

    Updated motion.

--- tribute-to-duchamp-anemic-cinema.js
+++ tribute-to-duchamp-anemic-cinema.js
@@ -1,28 +1,3 @@
-// Initial Prototype Drafted
-function setup() {
-  createCanvas(1000,1000);
-  smooth();
-  strokeWeight(1); // Default
-  line(20, 20, 80, 20);
-  strokeWeight(4); // Thicker
-  let min=100;
-  let max=600;
-  let step=20;
-  let rng=max-min;
-    for ( let r=max; r>=min; r+= -step) {
-      let x = random(min+(rng/2),min+(rng/2)+(rng/50));
-      let y = random(min+(rng/2),min+(rng/2)+(rng/50));
-      if ((r%(2*step))==0){
-        fill(255);
-      }
-        else {fill(0);}
-      ellipse(x, y, r,r);
-      
-    }
-}
-
-
-// With Motion; Trial I
 new p5();
 
 let min=100;
@@ -43,19 +18,21 @@ function setup() {
   strokeWeight(4); // Thicker
   }
 var angle = 0;
-var speed = 0.075;
+var speed = 0.0025;
 function draw(){
   let count = 0;
   for ( let r=max; r>=min; r+= -step) {
-    if ((r%(2*step))==0){
-        fill(255);
-      }
-        else {fill(0);}
     angle += speed;
     let sinval = sin(angle);
     let cosval = cos(angle);
-    let x = centers[count][0] + sinval*r;
-    let y = centers[count][1] + cosval*r;
+    if ((r%(2*step))==0){
+        fill(255);
+        var x = centers[count][0] + sinval*4;
+        var y = centers[count][1] + cosval*4;
+      }
+        else {fill(0);
+             var x = centers[count][0] + sinval*3;
+            var y = centers[count][1] + cosval*3;}   
     ellipse(x,y,r,r);
     count += 1;
   }
