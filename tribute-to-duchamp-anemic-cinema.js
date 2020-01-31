70cc03c75b4dd0a9bfbe5459537d296bebc0e456
Matthew-Mitchell <mmitchell17@protonmail.com>
2020-01-30 20:12:25 -0500

    Nicer looping.

--- tribute-to-duchamp-anemic-cinema.js
+++ tribute-to-duchamp-anemic-cinema.js
@@ -1,8 +1,8 @@
 new p5();
 
-let min=60;
+let min=32;
 let max=600;
-let step=20;
+let step=28;
 var centers=[];
 let rng=max-min;
 for ( let r=max; r>=min; r+= -step) {
@@ -17,7 +17,7 @@ function setup() {
   strokeWeight(2); // Default
   }
 var angle = 0;
-var speed = 0.005;
+var speed = 0.003;
 function draw(){
   let count = 0;
   // angle += speed;
@@ -25,14 +25,14 @@ function draw(){
     angle += (speed )
     let sinval = sin(angle);
     let cosval = cos(angle);
-    if ((r%(2*step))==0){
-        fill(0);
-        var x = centers[count][0] + sinval*4;
-        var y = centers[count][1] + cosval*4;
+    if (count%2==0){
+        fill(255);
+        var x = centers[count][0] + sinval*13;
+        var y = centers[count][1] + cosval*13;
       }
-        else {fill(255);
-             var x = centers[count][0] + sinval*3;
-            var y = centers[count][1] + cosval*3;}   
+        else {fill(0);
+             var x = centers[count][0] + sinval*6;
+            var y = centers[count][1] + cosval*6;}   
     ellipse(x,y,r,r);
     count += 1;
   }
